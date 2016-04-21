16ef6a533c31002226ca55e32003fd6d2450fb9e
akshayaurora <akshayaurora@gmail.com>
2016-04-22 00:27:29 +0530

    SDL2_vsync: rsync acc to sdl2 docs

--- kivy/config.py
+++ kivy/config.py
@@ -820,7 +820,7 @@ if not environ.get('KIVY_DOC_INCLUDE'):
                 'data/fonts/Roboto-BoldItalic.ttf'])
 
         elif version == 17:
-            Config.setdefault('graphics', 'vsync', 0)
+            Config.setdefault('graphics', 'vsync', 'None')
 
         # elif version == 1:
         #    # add here the command for upgrading from configuration 0 to 1
